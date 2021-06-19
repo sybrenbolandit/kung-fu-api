eccdac40996fe0ac63c701473495c029f84e03fe
sybrenbolandit <sybrenbolandit@gmail.com>
2021-06-19 10:32:57 +0200

    feat: Add random kick endpiont

---
+++ src/main/java/nl/sybrenbolandit/kungfu/kick/KickService.java
@@ -0,0 +1,22 @@
+package nl.sybrenbolandit.kungfu.kick;
+
+import javax.inject.Singleton;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Singleton
+public class KickService {
+
+    private static final List<Kick> KICKS = Arrays.asList(
+            new Kick("Front Kick"),
+            new Kick("Side Kick"),
+            new Kick("Roundhouse Kick"),
+            new Kick("Crescent Kick"),
+            new Kick("Axe Kick")
+    );
+
+    public Kick randomKick() {
+        return KICKS.get(new Random().nextInt(KICKS.size()));
+    }
+}
